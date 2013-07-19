dd7baf3bd179bb13c6d1d6b11996338f47ea67a7
SirAmNot <no_email@for.now>
2013-07-19 14:15:42 +0300

    Changed the sprite for the aqueous crystal

---
+++ dmi/siramnot/mods/dmi/BasicScroll.java
@@ -0,0 +1,13 @@
+package siramnot.mods.dmi;
+
+import net.minecraft.item.Item;
+
+public class BasicScroll extends Item {
+
+	public BasicScroll(int id) {
+		super(id);
+		// TODO Add code for basic scroll
+		// I made this class to avoid errors
+	}
+
+}
