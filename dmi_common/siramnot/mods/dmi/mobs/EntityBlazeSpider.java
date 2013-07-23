1724d77e27d5e1fa6c983b89900715f2da5786db
Tombenpotter <Tombenpotter@MagicScience>
2013-07-23 20:37:17 +0200

    BlazeSpider-ShootingTry1

--- dmi_common/siramnot/mods/dmi/mobs/EntityBlazeSpider.java
+++ dmi_common/siramnot/mods/dmi/mobs/EntityBlazeSpider.java
@@ -8,6 +8,8 @@ import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.SpiderEffectsGroupData;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntitySmallFireball;
 import net.minecraft.item.Item;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
@@ -89,4 +91,57 @@ import net.minecraft.world.World;
 	        this.playSound("mob.spider.step", 0.15F, 1.0F);
 	    }
 
+
+	
+	  protected void attackEntity(Entity par1Entity, float par2)
+	    {
+	        if (this.attackTime <= 0 && par2 < 2.0F && par1Entity.boundingBox.maxY > this.boundingBox.minY && par1Entity.boundingBox.minY < this.boundingBox.maxY)
+	        {
+	            this.attackTime = 20;
+	            this.attackEntityAsMob(par1Entity);
+	        }
+	        else if (par2 < 30.0F)
+	        {
+	            double d0 = par1Entity.posX - this.posX;
+	            double d1 = par1Entity.boundingBox.minY + (double)(par1Entity.height / 2.0F) - (this.posY + (double)(this.height / 2.0F));
+	            double d2 = par1Entity.posZ - this.posZ;
+
+	            if (this.attackTime == 0)
+	            {
+	                ++this.field_70846_g;
+
+	                if (this.field_70846_g == 1)
+	                {
+	                    this.attackTime = 60;
+	                    this.func_70844_e(true);
+	                }
+	                else if (this.field_70846_g <= 4)
+	                {
+	                    this.attackTime = 6;
+	                }
+	                else
+	                {
+	                    this.attackTime = 100;
+	                    this.field_70846_g = 0;
+	                    this.func_70844_e(false);
+	                }
+
+	                if (this.field_70846_g > 1)
+	                {
+	                    float f1 = MathHelper.sqrt_float(par2) * 0.5F;
+	                    this.worldObj.playAuxSFXAtEntity((EntityPlayer)null, 1009, (int)this.posX, (int)this.posY, (int)this.posZ, 0);
+
+	                    for (int i = 0; i < 1; ++i)
+	                    {
+	                        EntitySmallFireball entitysmallfireball = new EntitySmallFireball(this.worldObj, this, d0 + this.rand.nextGaussian() * (double)f1, d1, d2 + this.rand.nextGaussian() * (double)f1);
+	                        entitysmallfireball.posY = this.posY + (double)(this.height / 2.0F) + 0.5D;
+	                        this.worldObj.spawnEntityInWorld(entitysmallfireball);
+	                    }
+	                }
+	            }
+
+	            this.rotationYaw = (float)(Math.atan2(d2, d0) * 180.0D / Math.PI) - 90.0F;
+	            this.hasAttacked = true;
+	        }
+	    }
 }
