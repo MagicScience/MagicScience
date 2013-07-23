db7cadd27aa188975ab3a5516afb8935cc8cbc5e
SirAmNot <no_email@for.now>
2013-07-23 16:28:28 +0300

    Spell test update
    Spelltesting. Igneous crystal casts lightning bolts

---
+++ dmi_common/siramnot/mods/dmi/items/ItemStaff.java
@@ -0,0 +1,14 @@
+package siramnot.mods.dmi.items;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemTool;
+
+public class ItemStaff extends ItemTool {
+
+	public ItemStaff(int par1, float par2) {
+		super(par1, par2, null, null);
+	}
+
+}
